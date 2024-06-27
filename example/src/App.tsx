1d5172154d92a31177a1cd5878e89871b6bdb1fb
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2024-06-26 21:12:00 -0300

    feat(ios): add getMetadata function

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-get-metadata';
+import { getMetadata } from 'react-native-get-metadata';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    getMetadata('CFBundleDevelopmentRegion').then(setResult).catch(console.log);
   }, []);
 
   return (
