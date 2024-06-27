8c351499a991a6fc3b01fe64bd64f6d53e177877
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2024-06-27 11:18:08 -0300

    feat(android): add GetMetadata functions

--- android/src/main/java/com/getmetadata/GetMetadataModule.kt
+++ android/src/main/java/com/getmetadata/GetMetadataModule.kt
@@ -1,5 +1,8 @@
 package com.getmetadata
 
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
@@ -12,11 +15,33 @@ class GetMetadataModule(reactContext: ReactApplicationContext) :
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
+  fun getBundle(): Bundle {
+    val context = super.getReactApplicationContext()
+    val appInfo: ApplicationInfo = context.getPackageManager()
+      .getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA)
+    return appInfo.metaData
+  }
+
   @ReactMethod
-  fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
+  fun forKey(key: String, promise: Promise) {
+    try {
+      val bundle: Bundle = getBundle()
+      val value: String? = bundle.getString(key)
+      promise.resolve(value)
+    } catch (e: Throwable) {
+      promise.reject("[ERROR:GetMetadata.forKey]", e)
+    }
+  }
+
+  @ReactMethod
+  fun intForKey(key: String, promise: Promise) {
+    try {
+      val bundle: Bundle = getBundle()
+      val value: Int? = bundle.getInt(key)
+      promise.resolve(value)
+    } catch (e: Throwable) {
+      promise.reject("[ERROR:GetMetadata.intForKey]", e)
+    }
   }
 
   companion object {
